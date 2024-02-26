7e82fcc86cf69d32c719f57e4c79664ac967a3d3
rinijain123 <rinijain5646@gmail.com>
2024-02-25 20:15:40 -0800

    ...

--- site/new-react-frontend/src/pages/AIInsights.js
+++ site/new-react-frontend/src/pages/AIInsights.js
@@ -30,8 +30,8 @@ const AIInsights = () => {
 
   if (input != null) {
     // Add an event listener to the button that listens for a 'click' event
-    input.addEventListener('click', function(event) {
-      if (event === "Enter") {
+    input.addEventListener('keypress', function(event) {
+      if (event.key === "Enter") {
         // Insert the code you want to run when the button is clicked
         newBlueBubble(input);
       }
@@ -77,7 +77,7 @@ const AIInsights = () => {
           <div className="absolute h-[16.29%] w-[4.2%] top-[37.68%] right-[7.35%] bottom-[46.03%] left-[88.45%] rounded-[50%] bg-red" />
         </div>
         <div className="absolute top-[650px] left-[441px] w-[696px] h-[45px] text-left text-6xl text-dimgray">
-          <input name="input" type="text" placeholder='Type Here' maxlength="512" width="428px" id="userinput"/>
+          <input name="input" type="text" placeholder='Type Here' maxlength="512" width="1000px" id="userinput"/>
         </div>
         <div className="absolute top-[217px] left-[346px] w-[65px] h-[65px]">
           <img
